cb02bf365748e08bade6cb89e4a7ecd5a3f86cbb
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-12 02:48:10 +0200

    Add bootstrap

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "vendor/autoload.php";
 
 
@@ -7,16 +7,29 @@ require_once "vendor/autoload.php";
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <link rel="stylesheet" href="style.css">
+    <!-- bootstrap 5.0 start -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" defer integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" defer integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    <!-- bootstrap 5.0 end -->
+
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="container">
+
+
+</div>
+
+
+
 
 </body>
+
 </html>
